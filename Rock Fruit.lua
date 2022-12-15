563eed539f037d16516afb064af9284ba7e89453
fluk11380 <119399985+fluk11380@users.noreply.github.com>
2022-12-15 20:05:22 +0700

    Create Rock Fruit.lua

---
+++ Rock Fruit.lua
@@ -0,0 +1,47 @@
+local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
+local Window = Library.CreateLib("IGNITE-SV-X | Rock Fruit","DarkTheme")
+
+
+local Tab = Window:NewTab("วาปหาเกาะ")
+local a = Tab:NewSection("วาปหาเกาะ")
+local Tab2 = Window:NewTab("อื่นๆ")
+local b = Tab2:NewSection("อื่นๆ")
+---Tab1
+
+a:NewButton("เกาะแรก","...",function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(102.645164, 16.6120796, 338.617004, 0.996918023, 8.74142785e-08, 0.0784502476, -8.91678482e-08, 1, 1.88496774e-08, -0.0784502476, -2.57868233e-08, 0.996918023)
+end)
+
+a:NewButton("เกาะสุ่มผล","...",function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(809.163391, 6.6116972, -3.74305129, 0.953686476, -3.29538992e-08, -0.300802469, 2.09419841e-08, 1, -4.31572715e-08, 0.300802469, 3.48591058e-08, 0.953686476)
+end)
+
+a:NewButton("เกาะบัคกี้","...",function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-354.45929, 10.1199522, -47.9321365, 0.499479026, 0, 0.866325974, -0, 1.00000012, -0, -0.866326094, 0, 0.499478966)
+end)
+
+a:NewButton("เกาะเบค่อนตัวใหญ่","...",function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-307.894165, 7.33812141, 707.868103, -0.0236596968, 0, 0.999719977, 0, 0.99999994, -0, -0.999720097, 0, -0.0236596931)
+end)
+
+a:NewButton("เกาะเบค่อนแดงกับน้ำเงิน","...",function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(561.819336, 7.87747669, 1031.87622, -0.999234676, -0, -0.0391177051, -0, 1, -0, 0.0391177088, 0, -0.999234557)
+end)
+
+a:NewButton("เกาะขนมหวาน","...",function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1914.06348, 9.69719315, -173.401276, 0.13727361, 0, -0.990533173, -0, 1, -0, 0.990533173, 0, 0.13727361)
+end)
+
+a:NewButton("เกาะฟาร์มเวล","...",function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-820.529236, 19.2548656, 200.672592, 0.61040014, 0, 0.792093217, -0, 1, -0, -0.792093217, 0, 0.61040014)
+end)
+
+a:NewButton("เกาะดำทีโพส+หาออฟ","...",function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1512.79688, 57.0836983, 688.699036, -0.197581485, 3.12531263e-08, 0.980286479, -1.14507923e-08, 1, -3.41895863e-08, -0.980286479, -1.79802857e-08, -0.197581485)
+end)
+
+---Tab2
+
+b:NewButton("ออโต้คลิก(ไม่แนะนำแล้วแต่เลย",".....",function()
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/JustEzpi/ROBLOX-Scripts/main/ROBLOX_AutoClicker"))()
+end)
